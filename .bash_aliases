d1c21c42b1ac74136cc37be4a90bf34e4bb515c7
Chris <chris@trau.co>
2022-01-08 02:26:25 -0500

    removed old stuff

--- .bash_aliases
+++
@@ -1,3 +0,0 @@
-alias update='sudo apt-get update'
-alias config='/usr/bin/git --git-dir=/home/trauco/.cfg/ --work-tree=/home/trauco'
-
--- .oh-my-zsh/custom/python_dev.zsh
+++
@@ -1,7 +0,0 @@
-# Python Local Dev Workflows
-alias pyv3nv='python3 -m venv venv && source venv/bin/activate'
-alias DiePy='deactivate'
-
-# Pydriller
-alias pyD='pip3 install pydriller'
-
