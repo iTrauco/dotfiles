0ebdbd113d4e523888fc9d41c4a1c5529a6f7d53
Christopher Trauco <chris@trau.co>
2023-12-08 11:50:56 -0500

    jn ipython custom config start

---
+++ .ipython/profile_default/startup/custom.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from IPython import get_ipython
+ipython = get_ipython()
+
+#ipython.magic("pylab")
+#ipython.magic("load_ext autoreload")
+#ipython.magic("autoreload 2")
